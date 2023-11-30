a9e498f396569b0a27b9671fec4e6a59bf76e115
Дмитрий <d77s@inbox.ru>
2023-11-30 22:20:24 +0300

    Next

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+#  from django.shortcuts import render
+from rest_framework.mixins import (CreateModelMixin,
+                                   DestroyModelMixin,
+                                   ListModelMixin)
+from rest_framework import filters, viewsets
+from api.permissions import (IsAdminOrReadOnly)
+from api.serializers import (CategorySerializer,
+                             GenreSerializer)
+from reviews.models import (Category,
+                            Genre)
 
-# Create your views here.
+
+class CategoryViewSet(CreateModelMixin,
+                      ListModelMixin,
+                      DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenreViewSet(CreateModelMixin,
+                   ListModelMixin,
+                   DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
