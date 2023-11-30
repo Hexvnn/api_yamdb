aedda25fa6f713a58cf1ff33b911ec101d28e1cf
Семён Богданов <semenbogdanov2004@yandex.ru>
2023-11-30 22:48:02 +0300

    fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.BasePermissions):
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             (request.user.is_authenticated)
@@ -20,13 +20,13 @@ class IsAdmin(permissions.BasePermissions):
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS) or (
-            obj.author == request.user
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
         )
 
 
-class isOwner(permissions.BasePermissions):
+class isOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS) or (
             request.user.is_authenticated
