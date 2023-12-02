878b10bbcf15d22e4c117ef034a208c13ee90724
Андрей Тетнёв <andreytetnev@yandex.ru>
2023-12-02 19:39:39 +0300

    Merge brach feature/review in Develop

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,19 +3,27 @@ from rest_framework.routers import DefaultRouter
 from api.views import (CategoryViewSet,
                        GenreViewSet,
                        TitleViewSet,
+                       ReviewViewSet,
+                       CommentViewSet,
                        )
 
 router = DefaultRouter()
 router.register("categories", CategoryViewSet, basename="categories")
 router.register("genres", GenreViewSet, basename="genres")
 router.register("titles", TitleViewSet, basename="titles")
+router.register(
+    r"titles/(?P<title_id>\d+)/reviews",
+    ReviewViewSet,
+    basename="review"
+)
+router.register(
+    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
+    CommentViewSet,
+    basename="comments"
+)
 
 urlpatterns = [
     #  path("v1/auth/signup/", ...),
     #  path("v1/auth/token/", ...),
-    path('v1/auth/', include('djoser.urls')),  # !!!!!!!!!!!!!Это только для отладки части кода!!! потом удалить!!!
-    path('v1/auth/', include('djoser.urls.jwt')),  # !!!!!!!!!!!!!Это только для отладки части кода!!! потом удалить!!!
-    # login/pass admin1/admin1
-    # token access "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEwMTQ0OTY3LCJqdGkiOiI3ZmQwYThmYzZjNzg0MmIyYmM0YmM4MjJkZmZiNDEzOSIsInVzZXJfaWQiOjF9.Omga2Iawm19W9IiF1aLiT2EuInvQ93Gr-W2H7VEh1Qg"
     path("v1/", include(router.urls)),
 ]
