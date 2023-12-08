56a6b7c89c1c31b7d844c87d9a6f34d7d6e07891
Андрей Тетнёв <andreytetnev@yandex.ru>
2023-12-08 16:30:49 +0300

    fix comments and reviews

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from reviews.models import Category, Comment, Genre, Review, Title
 
@@ -74,13 +74,11 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         request = self.context.get("request")
+        if request.method != "POST":
+            return attrs
         author = request.user
         title_id = self.context.get("view").kwargs.get("title_id")
-        title = get_object_or_404(Title, pk=title_id)
-        if (
-            request.method == "POST"
-            and Review.objects.filter(author=author, title=title).exists()
-        ):
+        if Review.objects.filter(author=author, title_id=title_id).exists():
             raise serializers.ValidationError("Вы уже оставляли отзыв.")
         return attrs
 
